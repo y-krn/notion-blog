18b5eaf2836dc621b711e3b4235c740262387473
ottanjp <8692074+ottanjp@users.noreply.github.com>
2023-03-21 20:44:50 +0900

    Add class attribute to body tag.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='ja'>
       <Head />
-      <body>
+      <body className='antialiased text-slate-500 dark:text-slate-400 bg-white dark:bg-slate-900'>
         <Main />
         <NextScript />
       </body>
